fed7231d8bef4905f8bb8445f3d24a445db47d9f
egoing <egoing@gmail.com>
2015-04-14 09:45:16 +0900

    보안 수업 커밋

--- php/4.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", 111111);
-mysqli_select_db($conn, "opentutorials");
-$sql = "SELECT * FROM user WHERE pw='".$_GET['pw']."'";
-$row = mysqli_fetch_assoc($result);
-if($row["password"] == )
